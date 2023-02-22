da26bf1c3479f8f0d48dd96e38b4450a67dda929
sycct <huang_9119596@163.com>
2023-02-22 12:15:37 +0800

    2023-2-22 Fix the problem that there are multiple returned results.

--- app.py
+++ app.py
@@ -21,15 +21,17 @@ def get_hostname(ip):
     reverse_name = ip_address(ip).reverse_pointer
     uri = f"https://dns.google/resolve?name={reverse_name}&type=PTR"
     try:
-        get_result = requests.get(uri, timeout=1)
+        get_result = requests.get(uri, timeout=10)
     except (exceptions.ConnectionError, exceptions.HTTPError, exceptions.Timeout) as e:
         message = {'success': False, 'message': e}
         return jsonify(message)
     result_json = get_result.json()
     try:
-        get_answer = result_json['Answer'][0]
+        get_answer = result_json['Answer']
     except KeyError:
         return jsonify({'success': False, 'message': '解析出现错误！'})
+    answer_len = len(get_answer)
+    get_answer = get_answer[answer_len - 1] if answer_len > 1 else get_answer[0]
     get_type = get_answer['type']
     if get_type == 12:
         # 转换成域名形式
